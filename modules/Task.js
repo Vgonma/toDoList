00df0a14d96420f481ffaa43775530814fc44085
Vgonma <victorgmassimi.microverse@gmail.com>
2022-12-06 10:52:18 -0600

    Add ToDoList.js class

--- modules/Task.js
+++ modules/Task.js
@@ -5,17 +5,31 @@ export default class Task {
     this.index = index;
   }
 
-  set description(newDescription) { this.description = newDescription; }
+  set description(newDescription) {
+    this.description = newDescription;
+  }
 
-  set completed(newCompleted) { this.completed = newCompleted; }
+  set completed(newCompleted) {
+    this.completed = newCompleted;
+  }
 
-  set index(newIndex) { this.index = newIndex; }
+  set index(newIndex) {
+    this.index = newIndex;
+  }
 
-  get description() { return this.description; }
+  get description() {
+    return this.description;
+  }
 
-  get completed() { return this.completed; }
+  get completed() {
+    return this.completed;
+  }
 
-  get index() { return this.index; }
+  get index() {
+    return this.index;
+  }
 
-  toggleCompleted() { this.completed = !this.completed; }
+  toggleCompleted() {
+    this.completed = !this.completed;
+  }
 }
