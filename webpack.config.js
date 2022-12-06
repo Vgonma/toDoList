bd50c7bbb6e1829823ad43191523b53ceb75723e
Vgonma <victorgmassimi.microverse@gmail.com>
2022-12-06 10:40:53 -0600

    Setup linters and webpack

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+/* eslint-disable */
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+  modules: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  devServer: {
+    static: './dist',
+  },
+};
